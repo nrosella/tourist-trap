5a4b8afdf6dd5611b7cbd557d3adbac98e0a3760
apradhn <abhijit.pradhan6@gmail.com>
2015-07-27 17:50:03 -0400

    modified search functions to return hash with tourist trap info

--- app/models/yelp_tourist_trapper.rb
+++ app/models/yelp_tourist_trapper.rb
@@ -8,21 +8,42 @@ class YelpTouristTrapper
   RADIUS = 200
   LOCATION = "New York"
 
-  def self.search_by_coords(lat, lng)
+  def search_by_coords(lat, lng)
     params = {category_filter: CATEGORIES, radius_filter: RADIUS }
     coords = {latitude: lat, longitude: lng}
     results = Yelp.client.search_by_coordinates(coords, LOCALE, params)
+    tourist_traps = results.businesses
     trap_count = results.total
     trap_names = results.businesses.collect{|r| r.name}.join(", ")
-    "You are near #{trap_count} tourist traps: #{trap_names}."
+
+    puts "You are near #{trap_count} tourist traps: #{trap_names}."
+    build_data(tourist_traps)
   end
 
-  def self.search_by_neighborhood(neighborhood)
+  def search_by_neighborhood(neighborhood)
     params = {category_filter: CATEGORIES, radius_filter: RADIUS}
     results = Yelp.client.search(neighborhood, params, LOCALE)
+    tourist_traps = results.businesses
     trap_count = results.total
     trap_names = results.businesses.collect{|r| r.name}.join(", ")
-    "#{neighborhood} has #{trap_count} Tourist Traps, including #{trap_names}."
+    
+    puts "#{neighborhood} has #{trap_count} Tourist Traps, including #{trap_names}."
+    build_data(tourist_traps)
+  end
+
+  def build_data(tourist_traps)
+    tourist_traps.each.with
+    data[:neighborhoods] = get_neighborhoods(tourist_traps)
+    data[:landmarks] = get_landmarks(tourist_traps)
+    data
+  end
+
+  def get_neighborhoods(tourist_traps)
+    tourist_traps.collect { |trap| trap.location.neighborhoods }.flatten.uniq
+  end  
+
+  def get_landmarks(tourist_traps)
+    tourist_traps.select{|trap| trap.categories.flatten.include?("landmarks")}.collect{|trap| trap.name}
   end
 
 end
