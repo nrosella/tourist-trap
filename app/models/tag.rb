1398e3409b6146ce533ac7616753ef722507cbf8
beccaades <rebecca.ades@gmail.com>
2015-07-27 18:33:58 -0400

    added instagram api and media count for tage on tag.rb model

---
+++ app/models/tag.rb
@@ -0,0 +1,14 @@
+class Tag
+  attr_reader :client
+
+  def initialize
+    @client = Instagram.client(:client_id => ENV["instagram_client_id"])
+  end
+
+  def get_count_for_tag(tag)
+    results = @client.tag_search(tag)
+    results.collect {|result| result["media_count"]}.first
+    result.first
+  end
+
+end
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,4 @@
+Instagram.configure do |config|
+  config.client_id=ENV["instagram_client_id"]
+  config.client_secret=ENV["instagram_client_secret"]
+end
