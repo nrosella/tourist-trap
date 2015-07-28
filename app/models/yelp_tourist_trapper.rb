354d79d776fc1dbcfabdd3153d6db7b4be43b9d2
apradhn <abhijit.pradhan6@gmail.com>
2015-07-28 14:00:37 -0400

    changes data hash into instance variables of YelpTouristTrapper obj

--- app/models/yelp_tourist_trapper.rb
+++ app/models/yelp_tourist_trapper.rb
@@ -1,5 +1,6 @@
 class YelpTouristTrapper
-  attr_accessor :tourist_traps, :data
+  attr_accessor :tourist_traps, :data, :ticket_sales, :magicians, :tours, :landmarks, :gift_shops, :souvenirs,
+  :amusement_parks, :bike_rentals, :zoos, :aquariums, :boat_charters, :hotelstravel, :train_stations, :pedicabs
   include NeighborhoodParser::InstanceMethods
 
   CATEGORIES = [
@@ -21,12 +22,19 @@ class YelpTouristTrapper
     "21 Club", "Lenny's Pizza", "Café Lalo", "New York Public Library", "McGee's Pub"
   ]
 
+  def initialize
+    @coords = {}
+    @neighborhoods = []
+  end
+
   def search_by_coords(lat, lng)
     params = {category_filter: CATEGORIES, radius_filter: RADIUS }
     coords = {latitude: lat, longitude: lng}
     results = Yelp.client.search_by_coordinates(coords, LOCALE, params)
     self.tourist_traps = results.businesses
-    self.data = build_data
+    self.coords = {latitude: lat, longitude: lng}
+    self.neighborhoods = get_neighborhoods
+    build_data
   end
 
   def search_by_neighborhood(neighborhood)
@@ -34,30 +42,29 @@ class YelpTouristTrapper
     params = {category_filter: CATEGORIES, radius_filter: RADIUS}
     results = Yelp.client.search(neighborhood, params, LOCALE)
     self.tourist_traps = results.businesses
-    self.data = build_data
+    self.neighborhoods << neighborhoods
+    self.coords = get_coords
+    build_data
   end
 
   def build_data
-    data = {}
-    data[:neighborhoods] = get_neighborhoods
-    data[:landmarks] = get_category("landmarks")
-    data[:tours] = get_category("tours")
-    data[:magicians] = get_category("magicians")
-    data[:giftshops] = get_category("giftshops")
-    data[:souvenirs] = get_category("souvenirs")
-    data[:ticketsales] = get_category("ticketsales")
-    data[:amusementparks] = get_category("amusementparks")
-    data[:bikerentals] = get_category("bikerentals")
-    data[:zoos] = get_category("zoos")
-    data[:aquariums] = get_category("aquariums")
-    data[:boatcharters] = get_category("boatcharters")
-    data[:hotels] = get_category("hotelstravel")
-    data[:trainstations] = get_category("trainstations")
-    data[:pedicabs] = get_category("pedicabs")
-    data[:travelservices] = get_category("travelservices")
-    data[:localflavor] = get_category("localflavor")
-    data[:coords] = get_coords
-    data
+    self.neighborhoods = get_neighborhoods
+    self.landmarks = get_category("landmarks")
+    self.tours = get_category("tours")
+    self.magicians = get_category("magicians")
+    self.giftshops = get_category("giftshops")
+    self.souvenirs = get_category("souvenirs")
+    self.ticket_sales = get_category("ticketsales")
+    self.amusement_parks = get_category("amusementparks")
+    self.bike_rentals = get_category("bikerentals")
+    self.zoos = get_category("zoos")
+    self.aquariums = get_category("aquariums")
+    self.boat_charters = get_category("boatcharters")
+    self.hotels_travel = get_category("hotelstravel")
+    self.train_stations = get_category("trainstations")
+    self.pedicabs = get_category("pedicabs")
+    self.travel_services = get_category("travelservices")
+    self.local_flavor = get_category("localflavor")
   end
 
   def get_category(category)
