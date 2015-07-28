6cbe28eec8b9ce3a6bcf9213ed4646f70acdade5
apradhn <abhijit.pradhan6@gmail.com>
2015-07-28 13:25:59 -0400

    adds logic to parse neighborhood into neighborhood/city/state yelp format

--- app/helpers/neighborhood_parser.rb
+++ app/helpers/neighborhood_parser.rb
@@ -4,6 +4,15 @@ module NeighborhoodParser
   module InstanceMethods
     def parse_neighborhood(neighborhood)
       manhattan_neighborhoods = CSV.foreach("lib/neighborhoods/manhattan.csv").first
+      brooklyn_neighborhoods = CSV.foreach("lib/neighborhoods/brooklyn.csv").first
+      match = manhattan_neighborhoods.find{|m| m.downcase == neighborhood}
+      if (match.nil?)
+        match = brooklyn_neighborhoods.find{|b| b.downcase == neighborhood}
+        match = match + ", Brooklyn, NY"
+      else
+        match = match + ", Manhattan, NY"
+      end
+      match
     end
     
   end
