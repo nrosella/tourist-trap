3cef9fe2074d776b3f592741abb41adc33a7ec89
beccaades <rebecca.ades@gmail.com>
2015-07-28 13:34:56 -0400

    added tourists controller and create and index routes

--- app/controllers/tourists_controller.rb
+++ app/controllers/tourists_controller.rb
@@ -3,22 +3,19 @@ class TouristsController < ApplicationController
   def index  
   end
 
-  def create  
-  end
-
-  def show
-    @coords = YelpTouristTrapper.search_by_coords.find(coord_params)
-    @neighborhood = YelpTouristTrapper.search_by_neighborhood(neighborhood_params)
-    @tag_creator_neighborhood = TagCreator.convert_hash_to_tag(@neighborhood)
-    @tag_creator_coords = TagCreator.convert_hash_to_tag(@coords)
-    @istagram_tags_neighborhood = Tag.get_count_for_tag(@tag_creator_neighborhood)
-    @instagram_tags_coords = Tag.get_count_for_tag(@tag_creator_coords)
+  def create 
+    @coords = YelpTouristTrapper.new.search_by_coords(coord_params[:lat].to_f, coord_params[:lon].to_f)
+    @neighborhood = YelpTouristTrapper.new.search_by_neighborhood(neighborhood_params[:neighborhood])
+    @tag_creator_neighborhood = TagCreator.new.convert_hash_to_tag(@neighborhood)
+    @tag_creator_coords = TagCreator.new.convert_hash_to_tag(@coords)
+    @istagram_tags_neighborhood = Tag.new.get_count_for_tag(@tag_creator_neighborhood)
+    @instagram_tags_coords = Tag.new.get_count_for_tag(@tag_creator_coords)
   end
 
   private
 
   def coord_params
-    params.require(:tourist).permit(:lat, :lng)
+    params.require(:tourist).permit(:lat, :lon)
   end
 
   def neighborhood_params
