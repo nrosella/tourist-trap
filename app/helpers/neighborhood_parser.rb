ce1646cee15dce7ac0b5c0c4ef65c9c9a3b9ae17
apradhn <abhijit.pradhan6@gmail.com>
2015-07-29 14:15:58 -0400

    fixes bug due to capitalization

--- app/helpers/neighborhood_parser.rb
+++ app/helpers/neighborhood_parser.rb
@@ -5,9 +5,9 @@ module NeighborhoodParser
     def parse_neighborhood(neighborhood)
       manhattan_neighborhoods = CSV.foreach("lib/neighborhoods/manhattan.csv").first
       brooklyn_neighborhoods = CSV.foreach("lib/neighborhoods/brooklyn.csv").first
-      match = manhattan_neighborhoods.find{|m| m.downcase == neighborhood}
+      match = manhattan_neighborhoods.find{|m| m.downcase == neighborhood.downcase}
       if (match.nil?)
-        match = brooklyn_neighborhoods.find{|b| b.downcase == neighborhood}
+        match = brooklyn_neighborhoods.find{|b| b.downcase == neighborhood.downcase}
         match = match + ", Brooklyn, NY"
       else
         match = match + ", Manhattan, NY"
