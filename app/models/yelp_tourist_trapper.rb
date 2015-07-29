3c27b44e4a4de52206ba045ccde8441cc1d2477d
apradhn <abhijit.pradhan6@gmail.com>
2015-07-28 21:46:00 -0400

    refactors #build_famous_locations_data to not rely multiple calls to yelp api

--- app/models/yelp_tourist_trapper.rb
+++ app/models/yelp_tourist_trapper.rb
@@ -5,6 +5,7 @@ class YelpTouristTrapper
   attr_accessor :coords, :neighborhoods, :tourist_traps, :chains, :famous_locations
   attr_accessor :ticket_sales, :magicians, :tours, :landmarks, :gift_shops, :souvenirs,
   :amusement_parks, :bike_rentals, :zoos, :aquariums, :boat_charters, :hotels_travel, :train_stations, :pedicabs, :travel_services, :local_flavor
+
   include NeighborhoodParser::InstanceMethods
 
   LOCALE = {lang: "en", cc: "US"}
@@ -15,30 +16,6 @@ class YelpTouristTrapper
     @coords = {}   
     @neighborhoods = []    
   end 
-
-  def attributes
-    self.methods.select do |method|
-      /\w+={1}/.match(method)
-    end
-  end
-
-  def category_attributes
-    self.attributes.reject{|a| /coords=|neighborhoods=|tourist_traps=|chains=|famous_locations=/.match(a.to_s)}
-  end
-   
-  def self.categories    
-    CSV.foreach("app/models/tourist_traps/categories.csv").first.join(",")
-  end    
-   
-  def self.chains    
-    CSV.foreach("app/models/tourist_traps/chains.csv").first   
-  end    
-   
-  def self.famous_locations    
-    CSV.foreach("lib/assets/famous_locations.csv").with_object([]) do |row, arr|
-      arr << {name: row[0], latitude: row[1], longitude: row[2]}
-    end      
-  end  
 
   def search_by_coords(lat, lng)
     params = {category_filter: self.class.categories, radius_filter: RADIUS }
@@ -60,8 +37,33 @@ class YelpTouristTrapper
     self.neighborhoods << neighborhood
     self.coords = get_coords(results)
     build_data(neighborhood)
+  end  
+
+  def attributes
+    self.methods.select do |method|
+      /\w+={1}/.match(method)
+    end
   end
 
+  def category_attributes
+    self.attributes.reject{|a| /coords=|neighborhoods=|tourist_traps=|chains=|famous_locations=/.match(a.to_s)}
+  end
+   
+  def self.categories    
+    CSV.foreach("app/models/tourist_traps/categories.csv").first.join(",")
+  end    
+   
+  def self.chains    
+    CSV.foreach("app/models/tourist_traps/chains.csv").first   
+  end    
+   
+  def self.famous_locations    
+    CSV.foreach("lib/assets/famous_locations.csv").with_object([]) do |row, arr|
+      arr << {name: row[0], latitude: row[1].to_f, longitude: row[2].to_f}
+    end      
+  end  
+
+  private 
   def build_data(location)
     build_famous_locations_data
     build_chains_data(location)
@@ -71,7 +73,15 @@ class YelpTouristTrapper
 
   def build_famous_locations_data
     self.famous_locations = []
-    binding.pry
+    latitude = self.coords[:latitude]
+    longitude = self.coords[:longitude]
+    self.class.famous_locations.each do |fl|
+      params = [latitude, longitude, fl[:latitude], fl[:longitude]]
+      dist = GeoDistance.distance(*params)
+      if dist < RADIUS
+        self.famous_locations << fl[:name]
+      end
+    end
   end
 
   def build_chains_data(location)
@@ -105,7 +115,7 @@ class YelpTouristTrapper
   end  
 
   def get_coords(results)
-    {latitude: results.region.center.latitude, longitude: results.region.center.latitude}
+    {latitude: results.region.center.latitude, longitude: results.region.center.longitude}
   end
 
 end
