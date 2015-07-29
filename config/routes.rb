29fbddb7606ed8d3b5b8cfd8936ad055e5b93512
apradhn <abhijit.pradhan6@gmail.com>
2015-07-29 12:09:06 -0400

    creates Test Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'test/index'
+
   resources :tourists, only: [:create]
 
   # The priority is based upon order of creation: first created -> highest priority.
