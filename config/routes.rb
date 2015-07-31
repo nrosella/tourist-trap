00c9493574d25d712d222fafe3604bba31b7e6c0
nrosella <ninorosella@outlook.com>
2015-07-31 09:11:13 -0400

    initial team page added as well as some styling changes and jquery fixes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'test/index'
 
   resources :tourists, only: [:create]
+  get "/pages/:page" => "pages#show"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
